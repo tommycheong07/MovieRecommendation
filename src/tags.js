4154cc20c1ad583ac3404727dde3b5f3e85848a1
tommycheong07 <tommycheong98@gmail.com>
2020-08-14 00:04:34 -0700

    creating working tag textfield

--- src/tags.js
+++ src/tags.js
@@ -2,7 +2,7 @@ const tagContainer = document.querySelector('.tag-container');
 
 const input = document.querySelector('.tag-container input');
 
-
+var tags = [];
 
 function createTag(label) {
 	const div = document.createElement('div');
@@ -11,8 +11,9 @@ function createTag(label) {
 	const span = document.createElement('span');
 	span.innerHTML = label;
 
-	const closeBtn = document.createElement('span');
+	const closeBtn = document.createElement('i');
 	closeBtn.setAttribute('class', 'material-icons');
+	closeBtn.setAttribute('data-item', label);
 	closeBtn.innerHTML = 'close';
 
 	div.appendChild(span);
@@ -20,13 +21,35 @@ function createTag(label) {
 	return div;
 }
 
-tagContainer.prepend(createTag('Hello'))
+function reset() {
+	document.querySelectorAll('.tag').forEach(function(tag) {
+		tag.parentElement.removeChild(tag);
+	})
+}
 
+function addTags() {
+	reset();
+	tags.slice().reverse().forEach(function(tag) {
+		const input = createTag(tag);
+		tagContainer.prepend(input);
+	})
+}
 
 input.addEventListener('keyup', function(e) {
 	if (e.key === 'Enter') {
-		const tag = createTag(input.value);
-		tagContainer.prepend(tag);
+		alert("hello")
+		tags.push(input.value);
+		addTags();
 		input.value = '';
+	}
+})
+
+document.addEventListener('click', function(e){
+	if (e.target.tagName === 'I') {
+		const value = e.target.getAttribute('data-item');
+		const index = tags.indexOf(value);
+		tags = [...tags.slice(0, index), ...tags.slice(index + 1)]
+		console.log(tags)
+		addTags();
 	}
 })
