d1c98a1ff67b29bd07cb9dc9e19c873ce8548a34
tommycheong07 <tommycheong98@gmail.com>
2020-08-14 13:15:42 -0700

    added genre tag textfield as well as a search button

--- src/tags.js
+++ src/tags.js
@@ -1,12 +1,20 @@
-const tagContainer = document.querySelector('.tag-container');
+const actorTagContainer = document.querySelector('.actor-tag-container');
 
-const input = document.querySelector('.tag-container input');
+const actorInput = document.querySelector('.actor-tag-container input');
 
-var tags = [];
+const genreTagContainer = document.querySelector('.genre-tag-container');
 
-function createTag(label) {
+const genreInput = document.querySelector('.genre-tag-container input');
+
+const searchBtn = document.querySelector('button');
+
+var actorTags = [];
+
+var genreTags = [];
+
+function createActorTag(label) {
 	const div = document.createElement('div');
-	div.setAttribute('class', 'tag');
+	div.setAttribute('class', 'actorTag');
 
 	const span = document.createElement('span');
 	span.innerHTML = label;
@@ -21,35 +29,86 @@ function createTag(label) {
 	return div;
 }
 
-function reset() {
-	document.querySelectorAll('.tag').forEach(function(tag) {
-		tag.parentElement.removeChild(tag);
+function actorTagReset() {
+	document.querySelectorAll('.actorTag').forEach(function(actorTag) {
+		actorTag.parentElement.removeChild(actorTag);
 	})
 }
 
-function addTags() {
-	reset();
-	tags.slice().reverse().forEach(function(tag) {
-		const input = createTag(tag);
-		tagContainer.prepend(input);
+function addActorTags() {
+	actorTagReset();
+	actorTags.slice().reverse().forEach(function(actorTag) {
+		const input = createActorTag(actorTag);
+		actorTagContainer.prepend(input);
 	})
 }
 
-input.addEventListener('keyup', function(e) {
+actorInput.addEventListener('keyup', function(e) {
 	if (e.key === 'Enter') {
-		alert("hello")
-		tags.push(input.value);
-		addTags();
-		input.value = '';
+		actorTags.push(actorInput.value);
+		addActorTags();
+		actorInput.value = '';
+	}
+})
+
+function createGenreTag(label) {
+	const div = document.createElement('div');
+	div.setAttribute('class', 'genreTag');
+
+	const span = document.createElement('span');
+	span.innerHTML = label;
+
+	const closeBtn = document.createElement('b');
+	closeBtn.setAttribute('class', 'material-icons');
+	closeBtn.setAttribute('data-item', label);
+	closeBtn.innerHTML = 'close';
+
+	div.appendChild(span);
+	div.appendChild(closeBtn);
+	return div;
+}
+
+function genreTagReset() {
+	document.querySelectorAll('.genreTag').forEach(function(genreTag) {
+		genreTag.parentElement.removeChild(genreTag);
+	})
+}
+
+function addGenreTags() {
+	genreTagReset();
+	genreTags.slice().reverse().forEach(function(genreTag) {
+		const input = createGenreTag(genreTag);
+		genreTagContainer.prepend(input);
+	})
+}
+
+genreInput.addEventListener('keyup', function(e) {
+	if (e.key === 'Enter') {
+		genreTags.push(genreInput.value);
+		addGenreTags();
+		genreInput.value = '';
 	}
 })
 
 document.addEventListener('click', function(e){
 	if (e.target.tagName === 'I') {
 		const value = e.target.getAttribute('data-item');
-		const index = tags.indexOf(value);
-		tags = [...tags.slice(0, index), ...tags.slice(index + 1)]
-		console.log(tags)
-		addTags();
+		const index = actorTags.indexOf(value);
+		actorTags = [...actorTags.slice(0, index), ...actorTags.slice(index + 1)]
+		addActorTags();
+	}
+	if (e.target.tagName === 'B') {
+		const value = e.target.getAttribute('data-item');
+		const index = genreTags.indexOf(value);
+		genreTags = [...genreTags.slice(0, index), ...genreTags.slice(index + 1)]
+		addGenreTags();
 	}
 })
+
+searchBtn.addEventListener('click', event => {
+	alert("Button Clicked!")
+})
+
+
+
+
